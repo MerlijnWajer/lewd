5430175827e02697a2a627a49b793cbd7b868c75
Erik Bosman <erik@pizzadoos.com>
2012-10-02 00:06:57 +0200

    add 'slideshow' app

---
+++ src/show.py
@@ -0,0 +1,33 @@
+import led
+import sync
+import sys
+import time
+
+sys.path.append('..')
+
+
+import animations
+animations = [ x(12, 10) for x in animations.animations ]
+
+s = led.LedScreen()
+
+metronome = sync.Metronome(fps=25.)
+metronome.start()
+current = 0
+
+start = time.time()
+delay = 25.
+
+try:
+    while True:
+
+        current = int((time.time()-start)/delay) % len(animations)
+
+        s.push_frame(animations[current].next())
+
+        metronome.sync()
+
+finally:
+    pass
+    curses.reset_shell_mode()
+
