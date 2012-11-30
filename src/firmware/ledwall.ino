4a49332c4d0d6715c01f4e6f9cb4173de1633a15
Erik Bosman <erik@pizzadoos.com>
2012-11-30 02:57:57 +0100

    Simplify arduino code

--- src/firmware/ledwall.ino
+++ src/firmware/ledwall.ino
@@ -2,25 +2,24 @@
 
 void setup()
 {
-    Serial.begin(1000000);
-    SPI.begin();
-    SPI.setBitOrder(MSBFIRST);
-    SPI.setDataMode(SPI_MODE0);
-    SPI.setClockDivider(SPI_CLOCK_DIV16);
+	Serial.begin(1000000);
+	SPI.begin();
+	SPI.setBitOrder(MSBFIRST);
+	SPI.setDataMode(SPI_MODE0);
+	SPI.setClockDivider(SPI_CLOCK_DIV16);
 }
 
 void loop()
 {
-    uint8_t c;
+	uint8_t c;
 
 	for(;;)
 	{
 		while (!Serial.available()) {}
 
 		if ( (c = Serial.read()) == 254 )
-			break;
-
-        SPI.transfer(c);
+			delay(1);
+		else
+			SPI.transfer(c);
 	}
-    delay(1);
 }
