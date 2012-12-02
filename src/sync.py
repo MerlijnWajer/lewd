c8b46bad66c822be60f6b2465eebdae5f81bb12d
Merlijn Wajer <merlijn@wizzup.org>
2012-12-02 18:42:21 +0100

    Add GPL license.

--- src/sync.py
+++ src/sync.py
@@ -1,3 +1,23 @@
+"""
+  This file is part of the LEd Wall Daemon (lewd) project
+  Copyright (c) 2009-2012 by ``brainsmoke'' and Merlijn Wajer (``Wizzup'')
+
+    lewd is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    lewd is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with lewd.  If not, see <http://www.gnu.org/licenses/>.
+
+  See the file COPYING, included in this distribution,
+  for details about the copyright.
+"""
 import time
 
 class Metronome(object):
